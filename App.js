45196ca33e1b2c5274258e9eaa14f4290246bfd1
Anton <gusa0002@algonquinlive.com>
2024-09-25 03:09:00 -0400

    Finish practice exercise

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View, } from 'react-native';
 import { useState, useEffect } from 'react';
 import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import axios from 'axios'
+import axios from 'axios';
+import Feather from '@expo/vector-icons/Feather';
+
 
 
 
@@ -12,6 +14,8 @@ export default function App() {
     axios.get("https://jsonplaceholder.typicode.com/todos")
     .then((resp) => {
       setData(resp.data)
+      // console.log(resp.data);
+      
     })
     .catch(err => console.log("There is a fetch error"));
   }, []);
@@ -19,14 +23,53 @@ export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-        <Text>To Do List</Text>
+        <Text style={styles.title}>To Do List</Text>
         <FlatList
           data={data}
           keyExtractor={item => item.id}
           renderItem={({item}) => (
-            <View>
-              <Text>{item.id}. {item.title}</Text>
-              <Text>{item.completed}</Text>
+            <View style={styles.todo}>
+              <Text  style={styles.text}>{item.id}. {item.title}</Text>
+              {item.completed && <Feather name="check-square" size={24} color="black" onPress={() => {
+                let newArr = data.map(i => {
+                  if (i.id == item.id) {
+                    return {
+                        userId: i.userId,
+                        id: i.id,
+                        title: i.title,
+                        completed: false
+                    }
+                  }else {
+                    return {
+                      userId: i.userId,
+                      id: i.id,
+                      title: i.title,
+                      completed: i.completed
+                    }
+                  }
+                })
+                setData(newArr)
+              }}/>}
+              {!item.completed && <Feather name="square" size={24} color="black" onPress={() => {
+                let newArr = data.map(i => {
+                  if (i.id == item.id) {
+                    return {
+                        userId: i.userId,
+                        id: i.id,
+                        title: i.title,
+                        completed: true
+                    }
+                  }else {
+                    return {
+                      userId: i.userId,
+                      id: i.id,
+                      title: i.title,
+                      completed: i.completed
+                    }
+                  }
+                })
+                setData(newArr)
+              }} />}
             </View>
           )}
         />
@@ -43,6 +86,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    
-  }
+    fontSize: 35,
+    fontWeight:"bold",
+    marginHorizontal:"auto",
+    marginVertical: 5
+  },
+  todo: {
+    marginTop: 7,
+    marginHorizontal:5,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 20,
+    flexDirection: "row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    padding:10,
+  },
+  text: {
+    fontSize: 20,
+    maxWidth: 350
+  },
+
 });
